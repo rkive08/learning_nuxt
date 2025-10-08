29069b41b395b3a48489c1c1b72d82dc482be74d
ayuintennurillah <ayuintennurillah08@gmail.com>
2025-10-08 14:30:29 +0700

    learn add simple new page

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen bg-gray-50 text-gray-800">
+    <nav class="bg-blue-600 text-white p-4 flex justify-between">
+      <NuxtLink to="#" class="font-bold">Belajar Nuxt Js & Golang</NuxtLink>
+      
+    </nav>
+    <main class="p-8">
+      <slot />
+    </main>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-4xl font-bold text-blue-600">Dashboard Page</h1>
+  <div class="p-6">
+    <h1 class="text-2xl font-semibold mb-4">Welcome to Dashboard</h1>
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 class="text-4xl font-bold text-blue-600">
+        Hello Tailwind + Nuxt 4 🎉
+      </h1>
+    </div>
+  </div>
+</template>
