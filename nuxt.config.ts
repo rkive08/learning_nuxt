443045d8f197985bc4ba713ca794c43cad723b6a
ayuintennurillah <ayuintennurillah08@gmail.com>
2025-10-08 15:30:09 +0700

    feat(config): add runtimeConfig to use API endpoint from .env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+   runtimeConfig: {
+  public: {
+    API_ENDPOINT: process.env.NUXT_PUBLIC_API_ENDPOINT,
+  },
+},
+  routeRules: {
+    '/api/**': {
+      proxy: {
+        to: `${process.env.NUXT_PUBLIC_API_ENDPOINT}/api/**`,
+      },
+    },
+  },
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
